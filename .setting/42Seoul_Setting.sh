96daa668e8a5d65bbf4bf0585e1960bdbc812f5a
seunan <seunan@student.42seoul.kr>
2024-01-06 20:58:11 +0900

    Modify Setting shell script

--- .setting/42Seoul_Setting.sh
+++ .setting/42Seoul_Setting.sh
@@ -6,6 +6,14 @@ else
   echo "- the directory added. : ~/goinfre/42cursus"
 fi
 
+if [[ -d "$HOME/goinfre/blog" ]]
+then
+  echo "- the directory exists. : ~/goinfre/blog"
+else
+  git clone https://github.com/seungwonme/seungwonme.github.io.git $HOME/goinfre/blog
+  echo "- the directory added. : ~/goinfre/blog"
+fi
+
 if [[ -d "$HOME/goinfre/.brew" ]]
 then
   echo "- the directory exists. : ~/goinfre/.brew"
